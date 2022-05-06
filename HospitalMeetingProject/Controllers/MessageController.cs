ebdc4b7086380cec005fa5fe9d74d282a39c97c7
ertugrulocakee <ertugrulocakee@gmail.com>
2022-05-06 12:25:43 +0300

    Gelen İletişim Mesajları Sayfası eklendi.

---
+++ HospitalMeetingProject/Controllers/MessageController.cs
@@ -0,0 +1,44 @@
+﻿using BLL.Concrete;
+using DAL.EF;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalMeetingProject.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class MessageController : Controller
+    {
+
+        ContactManager contactManager = new ContactManager(new EFContactDAL());
+
+        public IActionResult Index()
+        {
+            var values = contactManager.TGetList();
+
+            return View(values);
+
+        }
+
+        public IActionResult MessageDetail(int id)
+        {
+
+            var value = contactManager.TGetByID(id);    
+
+            return View(value); 
+
+        }
+
+        public IActionResult DeleteMessage(int id)
+        {
+
+            var value = contactManager.TGetByID(id);
+
+            contactManager.TDelete(value);
+
+            return RedirectToAction("Index"); 
+        }
+
+
+
+    }
+}
